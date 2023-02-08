f32a6532d52b409918a62f209ae5a79646f5c054
David Wang <devtesting404@gmail.com>
2023-02-09 01:13:05 +0800

    Add websev.hpp

---
+++ webserv.hpp
@@ -0,0 +1,18 @@
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <exception>
+#include <fstream>
+#include <ios>
+
+#define BACKLOG 5
+#define PORT 8080
+
+void ft_perror(const std::string &str);
+
+#endif
