d38ccc224531c76184d2586019ab3905d57421f9
David Wang <devtesting404@gmail.com>
2023-02-14 16:45:24 +0800

    std::endl after printing error string

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -2,13 +2,15 @@
 #include <cstring>
 #include <cerrno>
 #include <sstream>
+#include <utility>
+#include <string>
 
 std::string ret_str_perror(const std::string &msg)
 {
 	std::stringstream ss;
 	std::string str;
 
-	ss << msg << ": " << std::strerror(errno);
+	ss << msg << ": " << std::strerror(errno) << std::endl;
 	str = ss.str();
 	return str;
 }
