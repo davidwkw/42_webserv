af6948a234ff4fd0336b20e8a7c56d2a365b9d21
David Wang <devtesting404@gmail.com>
2023-02-16 00:44:26 +0800

    Add colors to messages in logger class

--- includes/Logger.hpp
+++ includes/Logger.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <type_traits>
+#include "colors.hpp"
 
 class Logger{
 	private:
@@ -20,42 +21,43 @@ class Logger{
 		~Logger();
 		Logger(const std::string &path, std::ostream *default_stream = &std::cout);
 
-		template <class T>
+		template < class T >
 		void warn(const T &msg) const;
 
-		template <class T>
+		template < class T >
 		void error(const T &msg) const;
 
-		template <class T>
+		template < class T >
 		void info(const T &msg) const;
 
-		template <class T>
+		template < class T >
 		void debug(const T &msg) const;
 
 		bool isDefaultStream() const;
 };
 
-template <class T>
+template < class T >
+void Logger::info(const T &msg) const {
+	(*_log_stream) << BOLD << BLUE << "[Info] " << RESET << msg << std::endl;
+}
+
+template < class T >
 void Logger::debug(const T &msg) const{
-	(*_log_stream) << "[Debug] " << msg << std::endl;
+	(*_log_stream) << BOLD << GREEN << "[Debug] " << RESET << msg << std::endl;
 }
 
-template <class T>
+template < class T >
 void Logger::warn(const T &msg) const {
-	(*_log_stream) << "[Warning] " << msg << std::endl;
+	(*_log_stream) << BOLD << YELLOW << "[Warning] " << RESET << msg << std::endl;
 }
 
-template <class T>
+template < class T >
 void Logger::error(const T &msg) const {
 	if (_log_stream == &std::cout)
-		std::cerr << "[Error] " << msg << std::endl;
+		std::cerr << BOLD << RED << "[Error] " << msg << std::endl;
 	else
-		(*_log_stream) <<  "[Error] " << msg << std::endl;
+		(*_log_stream) << BOLD << RED << "[Error] " << RESET << msg << std::endl;
 }
 
-template <class T>
-void Logger::info(const T &msg) const {
-	(*_log_stream) << "[Info] " << msg << std::endl;
-}
 
 #endif
