e28d33d8bb706540d28753432ae256a3b77692b6
David Wang <devtesting404@gmail.com>
2023-02-27 16:51:01 +0800

    Add set lookup for directive defaults

--- includes/BaseConfig.hpp
+++ includes/BaseConfig.hpp
@@ -4,22 +4,96 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <set>
 #include "webserv.hpp"
 
 class BaseConfig{
 	protected:
-		std::map<std::string, std::string> _directives;
+		enum Actions{
+			APPEND_EXISTING,
+			INSERT_NEW,
+			OVERRIDE,
+			DUPLICATE_ERROR
+		};
+
+		enum DirectiveStatus{
+			DEFAULT,
+			INHERITED,
+			DEFINED
+		};
+	
+		typedef std::map<std::string, std::pair<std::string, DirectiveStatus> > directive_container_type;
+		typedef typename directive_container_type::value_type					directive_type;
+		typedef typename directive_container_type::mapped_type					directive_value_type;
+		
+		static const char 								*simple_directive_array[];
+		static const char								*array_directive_array[];
+		static const char 								*block_directive_array[];
+		static const std::set<std::string> 				_simple_directives_set;
+		static const std::set<std::string> 				_array_directives_set;
+		static const std::set<std::string>				_block_directives_set;
+		static const std::map<std::string, Actions>		_directive_actions;
+		static const std::map<std::string, std::string>	directive_defaults;
+
+		static std::map<std::string, std::string> _init_directive_defaults();
+
+		template <std::size_t N>
+		static std::set<std::string> _init_directive_set(const char *(&arr)[N]);
+		
+		directive_container_type _directives;
+
+	private:
+		static std::map<std::string, Actions> _fill_directive_actions();
+
 	public:
 		BaseConfig();
 		virtual ~BaseConfig();
 		BaseConfig(const BaseConfig &ref);
 		BaseConfig &operator=(const BaseConfig &ref);
-		BaseConfig(const std::map<std::string,std::string> &directives);
+		BaseConfig(const directive_container_type &directives);
 
-		std::map<std::string, std::string> directives() const;
-		void add_directive(std::pair<std::string, std::string> directive);
+		template <class Iterator>
+		BaseConfig(const directive_container_type &directives, Iterator first, Iterator last,
+				typename enable_if<is_same<typename remove_const<typename Iterator::value_type>::type, std::string>::value ||
+								   is_same<typename remove_const<typename Iterator::value_type>::type, char*>::value>::type* = nullptr);
 		
-		static std::pair<std::string, std::string> parse_simple_directive(const std::string &directive);
+		directive_container_type directives() const;
+		void add_directive(directive_type directive);
+		
+		static directive_type parse_simple_directive(const std::string &directive, BaseConfig::DirectiveStatus status = DEFINED);
+
+		
 };
+
+template <class Iterator>
+BaseConfig::BaseConfig(const directive_container_type &directives, Iterator first = BaseConfig::_valid_directives.begin(), Iterator last = BaseConfig::_valid_directives.begin(),
+				typename enable_if<is_same<typename remove_const<typename Iterator::value_type>::type, std::string>::value
+								|| is_same<typename remove_const<typename Iterator::value_type>::type, char*>::value>::type* = nullptr){
+	Iterator first_copy = first;
+	
+	this->_directives = directives;
+	for (typename directive_container_type::iterator directives_start = directives.begin(); directives_start != directives.end(); ++directives_start)
+	{
+		while (first != last)
+		{
+			if (*directives_start == *first)
+				break;
+			++first;
+		}
+		if (first == last)
+			this->_directives.erase(*directives_start);
+		first = first_copy;
+	}
+}
+
+template <std::size_t N>
+std::set<std::string> BaseConfig::_init_directive_set(const char *(&arr)[N]){
+
+	std::set<std::string> fill_set;
+
+	for (std::size_t i = 0; i < N; i++)
+		fill_set.insert(arr[i]);
+	return fill_set;
+}
 
 #endif
--- srcs/BaseConfig.cpp
+++ srcs/BaseConfig.cpp
@@ -1,45 +1,141 @@
 #include "BaseConfig.hpp"
 
-BaseConfig::BaseConfig() : _directives(){
+const char *BaseConfig::simple_directive_array[] = {
+											"server",
+											"location",
+											"error_log",
+											"server_name",
+											"index",
+											"error_page",
+											"listen",
+											"return",
+											"cgi",
+											"user",
+											"pid",
+											"worker_processes",
+											"autoindex",
+											"root",
+											"client_body_temp_path",
+											"try_files",
+											"client_max_body_size",
+											"limit_except"
+											};
+									
+const char *BaseConfig::block_directive_array[] =	{
+													"server",
+													"location",
+													"limit_except"
+													};
 
+const char *BaseConfig::array_directive_array[] =	{
+													"error_log",
+													"server_name",
+													"listen"
+													};
+
+
+const std::set<std::string> BaseConfig::_simple_directives_set = BaseConfig::_init_directive_set(BaseConfig::simple_directive_array);
+
+const std::set<std::string> BaseConfig::_array_directives_set = BaseConfig::_init_directive_set(BaseConfig::array_directive_array);
+
+const std::set<std::string> BaseConfig::_block_directives_set = BaseConfig::_init_directive_set(BaseConfig::block_directive_array);
+
+const std::map<std::string, BaseConfig::Actions> BaseConfig::_directive_actions = BaseConfig::_fill_directive_actions();
+
+std::map<std::string, BaseConfig::Actions> BaseConfig::_fill_directive_actions(){
+	typedef std::pair<std::string, BaseConfig::Actions> directive_action_pair;
+	
+	const directive_action_pair directives_actions[]	=	{
+												directive_action_pair("server_name", APPEND_EXISTING),
+												directive_action_pair("error_log", APPEND_EXISTING),
+												directive_action_pair("index", APPEND_EXISTING),
+												directive_action_pair("error_page", INSERT_NEW), // needs more testing..
+												directive_action_pair("listen", INSERT_NEW), // but will throw error if a port is already defined?
+												directive_action_pair("return", INSERT_NEW), // will process the first return
+												directive_action_pair("cgi", INSERT_NEW),
+												directive_action_pair("user", DUPLICATE_ERROR),
+												directive_action_pair("pid", DUPLICATE_ERROR),
+												directive_action_pair("worker_processes", DUPLICATE_ERROR),
+												directive_action_pair("autoindex", DUPLICATE_ERROR),
+												directive_action_pair("root", DUPLICATE_ERROR),
+												directive_action_pair("client_body_temp_path", DUPLICATE_ERROR),
+												directive_action_pair("try_files", DUPLICATE_ERROR),
+												directive_action_pair("client_max_body_size", DUPLICATE_ERROR),
+												directive_action_pair("limit_except", DUPLICATE_ERROR)
+												};
+
+	std::map<std::string, BaseConfig::Actions> fill_map;
+
+	for (std::size_t i = 0; i < sizeof(directives) / sizeof(std::string); i++)
+		fill_map.insert(directives_actions[i]);
+	return fill_map;
 }
 
-BaseConfig::~BaseConfig(){
+const std::map<std::string, std::string> BaseConfig::directive_defaults = BaseConfig::_init_directive_defaults();
 
+std::map<std::string, std::string> BaseConfig::_init_directive_defaults(){
+	typedef std::pair<std::string, std::string> directive_default_pair;
+	
+	const directive_default_pair directives_actions[]	=	{
+												directive_default_pair("server_name", ""),
+												directive_default_pair("error_log", "logs/error.log error"),
+												directive_default_pair("index", "index.html"),
+												directive_default_pair("listen", "*:80 | *:8000"),
+												directive_default_pair("user", "nobody nobody"),
+												directive_default_pair("pid", "logs/nginx.pid"),
+												directive_default_pair("worker_processes", "1"),
+												directive_default_pair("autoindex", "off"),
+												directive_default_pair("root", "html"),
+												directive_default_pair("client_body_temp_path", "client_body_temp"),
+												directive_default_pair("client_max_body_size", "1m")
+												};
+
+	std::map<std::string, std::string> fill_map;
+
+	for (std::size_t i = 0; i < sizeof(directives) / sizeof(std::string); i++)
+		fill_map.insert(directives_actions[i]);
+	return fill_map;
 }
+
+BaseConfig::BaseConfig() : _directives(){}
+
+BaseConfig::~BaseConfig(){}
 
 BaseConfig::BaseConfig(const BaseConfig &ref) : _directives(ref._directives){
 	*this = ref;
 }
 
 BaseConfig &BaseConfig::operator=(const BaseConfig &ref){
-	if (this != &ref)
-	{
+	if (this != &ref){
 		this->_directives = ref._directives;
 	}
 	return *this;
 }
 
-BaseConfig::BaseConfig(const std::map<std::string,std::string> &directives){
+BaseConfig::BaseConfig(const directive_container_type &directives){
 	this->_directives = directives;
 }
 
-std::map<std::string, std::string> BaseConfig::directives() const{
+BaseConfig::directive_container_type BaseConfig::directives() const{
 	return this->_directives;
 }
 
-void BaseConfig::add_directive(std::pair<std::string, std::string> directive){
+void BaseConfig::add_directive(directive_type directive){
 	this->_directives.insert(directive);
 }
 
-std::pair<std::string, std::string> BaseConfig::parse_simple_directive(const std::string &directive){
-	std::stringstream temp(directive);
-	std::string k;
-	std::string v;
-	std::string word;
+BaseConfig::directive_type BaseConfig::parse_simple_directive(const std::string &directive, BaseConfig::DirectiveStatus status = DEFINED){
+	std::stringstream 				temp;
+	std::string 					k;
+	std::string 					word;
+	BaseConfig::directive_value_type result_value;
 
+	temp.str(trim_ws_str(directive));
 	temp >> k;
-	v = trim_ws_str(temp.str());
+	while (temp >> word){
+		result_value.first += (result_value.first.empty() ? "" :  " " ) + word;
+	}
+	result_value.second = status;
 	
-	return std::make_pair(k, v);
+	return std::make_pair(k, result_value);
 }
