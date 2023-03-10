16d3ddca26e07b042db08fed87d57189f23aaa7c
David Wang <devtesting404@gmail.com>
2023-03-10 19:17:16 +0800

    Commit LimitExcept class

---
+++ includes/LimitExcept.hpp
@@ -0,0 +1,31 @@
+#ifndef __LIMIT_EXCEPT_HPP__
+#define __LIMIT_EXCEPT_HPP__
+
+#include <string>
+#include <vector>
+#include "BaseConfig.hpp"
+#include "utils.hpp"
+
+class LimitExcept : public BaseConfig{
+	private:
+		static const char 								*all_directives_array[];
+		static const char 								*normal_directives_array[];
+		static const char								*array_directives_array[];
+		static const char 								*block_directives_array[];
+		static const std::set<std::string> 				all_directives_set;
+		static const std::set<std::string> 				normal_directives_set;
+		static const std::set<std::string> 				array_directives_set;
+		static const std::set<std::string>				block_directives_set;
+
+	public:	
+		LimitExcept();
+		~LimitExcept();
+		LimitExcept(BaseConfig::directive_container_type directives, const std::string &str);
+		LimitExcept(const LimitExcept &ref);
+		LimitExcept &operator=(const LimitExcept &ref);
+
+		const std::vector<std::string> &allow() const;
+		const std::vector<std::string> &deny() const;
+};
+
+#endif
